f8335ac39e8eb7c5ddd97f04622adb8e442f7ef7
adithyanhp <adithyanhpm@gmail.com>
2025-11-24 23:48:39 +0530

    Updated project files by adding Plans.jsx file inside the components

--- client/src/components/Plans.jsx
+++ client/src/components/Plans.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Plans = () => {
   return (
-    <div>
-      
+    <div className='max-w-2xl mx-auto z-20 my-30 max-md:px-4'>
+      <div className='text-center'>
+        <h2 className='text-gray-700 text-4xl font-semibold'>Choose Your Plan</h2>
+        <p>Start for free and scale up as you grow. Find the perfect plan for your content creation needs.</p>
+      </div>
     </div>
   )
 }
