c43ca688f49c353aa454b1baec4a282485f2f8ed
adithyanhp <adithyanhpm@gmail.com>
2025-11-30 23:33:44 +0530

    Updated marketplace file

--- client/src/pages/Marketplace.jsx
+++ client/src/pages/Marketplace.jsx
@@ -1,9 +1,23 @@
+import { ArrowLeftIcon } from 'lucide-react'
 import React from 'react'
+import {useNavigate} from 'react-router-dom'
 
 const Marketplace = () => {
+
+  const navigate = useNavigate()
+
   return (
-    <div>
-      <h1>Marketplace Page</h1>
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32'>
+
+      <div className='flex items-center justify-between text-slate-500'>
+        <button onClick={()=>{navigate('/')}}>
+          <ArrowLeftIcon className="size=4"/>
+          Back to Home
+        </button>
+        <button>Filters</button>
+      </div>
+
+      <div></div>
     </div>
   )
 }
